7d6932de5312136fed5fc74163ee7cbb44ce6b70
Grover101 <limachigrover09@gmail.com>
2023-02-01 08:16:31 -0400

    add integrate router and DB

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,20 @@
 import 'dotenv/config'
-import express, { type Response, type Request, Router } from 'express'
+import express from 'express'
 import cors from 'cors'
-import mongoose from 'mongoose'
+import { router } from '@routes/index'
+import dbConnect from '@config/mongo'
 
-const PORT = process.env.PORT ?? 3000
+const PORT: number = !Number.isNaN(process.env.PORT)
+    ? Number(process.env.PORT)
+    : 9000
 const app = express()
 app.use(cors())
+app.use('/api/v1', router)
 
-mongoose
-    .connect(`mongodb://${process.env.HOST_MONGO ?? 'localhost'}/prueba`)
-    .then(db => {
-        console.log('DB is connected to', db.connection.host)
-    })
-    .catch(error => {
-        console.log('error:', error)
-    })
-
-const router = Router()
-
-router.get('/api', (req: Request, res: Response) => {
-    res.status(200).json({ message: 'Hello World!!!' })
+void dbConnect().then(() => {
+    console.log('DB is connected to mongo')
 })
 
-app.use('/', router)
-
 app.listen(PORT, () => {
-    console.log(
-        `Server is running on port http://${
-            process.env.HOST ?? 'localhost'
-        }:${PORT}`
-    )
+    console.log(`Server is running on port http://${process.env.HOST}:${PORT}`)
 })
