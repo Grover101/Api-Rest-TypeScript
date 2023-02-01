3d1e5b0452434f9dd99acc9c4d269e65c23526c3
Grover101 <limachigrover09@gmail.com>
2023-02-01 10:16:19 -0400

    add method in controller Tweet

--- src/controllers/ControllerTweet.ts
+++ src/controllers/ControllerTweet.ts
@@ -1,29 +1,30 @@
+import { getTweetId, getTweets, insertTweet } from '@services/tweet.services'
 import { handleHttp } from '@utils/error.handle'
 import { type Request, type Response } from 'express'
 
 export default {
-    index(_req: Request, res: Response) {
+    async index(_req: Request, res: Response) {
         try {
-            const option = 'hola'
-            return res.status(200).json({ all: option })
+            const tweets = await getTweets()
+            return res.status(200).json(tweets)
         } catch (error) {
             handleHttp(res, 'Error in the request')
         }
     },
-    show(_req: Request, res: Response) {
+    async show({ params }: Request, res: Response) {
         try {
-            const option = 'hola'
-            return res.status(200).json({ all: option })
+            const tweet = await getTweetId(params.id)
+            return res.status(200).json(tweet)
         } catch (error) {
             handleHttp(res, 'Error in the request')
         }
     },
-    create(_req: Request, res: Response) {
+    async create({ body }: Request, res: Response) {
         try {
-            const option = 'hola'
-            return res.status(200).json({ all: option })
+            const tweet = await insertTweet(body)
+            return res.status(200).json(tweet)
         } catch (error) {
-            handleHttp(res, 'Error in the request')
+            handleHttp(res, 'Error in the request', error)
         }
     },
     createLike(_req: Request, res: Response) {
