9330ee3c801a60ca8452d4eb2b27d5ea381c7124
Grover101 <limachigrover09@gmail.com>
2023-02-01 19:40:32 -0400

    controller tweet finalize

--- src/controllers/ControllerTweet.ts
+++ src/controllers/ControllerTweet.ts
@@ -1,4 +1,10 @@
-import { getTweetId, getTweets, insertTweet } from '@services/tweet.services'
+import {
+    deleteTweet,
+    getTweetId,
+    getTweets,
+    insertTweet,
+    updateTweet
+} from '@services/tweet.services'
 import { handleHttp } from '@utils/error.handle'
 import { type Request, type Response } from 'express'
 
@@ -14,8 +20,13 @@ export default {
     async show({ params }: Request, res: Response) {
         try {
             const tweet = await getTweetId(params.id)
+            const data = tweet !== undefined ? tweet : 'Not Found'
+            console.log('Show: ', data)
+
             return res.status(200).json(tweet)
         } catch (error) {
+            console.log(error)
+
             handleHttp(res, 'Error in the request')
         }
     },
@@ -35,18 +46,22 @@ export default {
             handleHttp(res, 'Error in the request')
         }
     },
-    update(_req: Request, res: Response) {
+    async update({ body, params }: Request, res: Response) {
         try {
-            const option = 'hola'
-            return res.status(200).json({ all: option })
+            const tweet = await updateTweet(params.id, body)
+            return res.status(200).json(tweet)
         } catch (error) {
             handleHttp(res, 'Error in the request')
         }
     },
-    delete(_req: Request, res: Response) {
+    async delete({ params }: Request, res: Response) {
         try {
-            const option = 'hola'
-            return res.status(200).json({ all: option })
+            const tweet = await deleteTweet(params.id)
+            return tweet
+                ? res.status(200).json({ message: 'Tweet deleted' })
+                : res.status(404).json({
+                      message: 'No record found'
+                  })
         } catch (error) {
             handleHttp(res, 'Error in the request')
         }
