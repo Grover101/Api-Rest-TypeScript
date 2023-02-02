4ea3688dfe4b2a3101d43c9358249c425b13f804
Grover101 <limachigrover09@gmail.com>
2023-02-02 16:32:39 -0400

    fix message error

--- src/utils/error.handle.ts
+++ src/utils/error.handle.ts
@@ -1,9 +1,7 @@
 import { type Response } from 'express'
 
-const handleHttp = (res: Response, error: string, errorRaw?: any): unknown => {
-    console.log(typeof errorRaw)
-
-    return res.status(500).json({ message: error, error: errorRaw?._message })
+const handleHttp = (res: Response, error: string, errorRaw?: any): Response => {
+    return res.status(500).json({ message: error, error: errorRaw })
 }
 
 export { handleHttp }
