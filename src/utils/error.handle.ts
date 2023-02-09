fe4e4e0b59b9d11ac1bf604c623073d0aeea5631
Grover101 <limachigrover09@gmail.com>
2023-02-09 17:28:38 -0400

    change type error param

--- src/utils/error.handle.ts
+++ src/utils/error.handle.ts
@@ -1,6 +1,10 @@
 import { type Response } from 'express'
 
-const handleHttp = (res: Response, error: string, errorRaw?: any): Response => {
+const handleHttp = (
+    res: Response,
+    error: string,
+    errorRaw?: unknown
+): Response => {
     return res.status(500).json({ message: error, error: errorRaw })
 }
 
