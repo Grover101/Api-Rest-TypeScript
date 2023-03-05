2a2da145736e50361fcc421f2a640d12da088edf
Grover101 <limachigrover09@gmail.com>
2023-03-05 12:35:54 -0400

    add function encryptPassword in model User

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,8 +1,9 @@
-import { getModelForClass, prop } from '@typegoose/typegoose'
+import { DocumentType, getModelForClass, prop } from '@typegoose/typegoose'
 import { TimeStamps } from '@typegoose/typegoose/lib/defaultClasses'
+import bcrypt from 'bcryptjs'
 
 export class User extends TimeStamps {
-    @prop({ type: String, required: true })
+    @prop({ type: String, default: '' })
     name: string
 
     @prop({ type: String, unique: true, required: true, trim: true })
@@ -25,6 +26,10 @@ export class User extends TimeStamps {
 
     @prop({ type: String, required: true, minlength: 8 })
     password: string
+
+    public async encryptPassword(this: DocumentType<User>, password: string) {
+        this.password = await bcrypt.hash(password, 10)
+    }
 }
 
 const UserModel = getModelForClass(User)
