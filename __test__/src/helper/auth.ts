4a5471c45eb3dc7d9284894c00e153f02da5e597
Grover101 <limachigrover09@gmail.com>
2023-03-05 12:39:46 -0400

    add data user init, login and token

---
+++ __test__/src/helper/auth.ts
@@ -0,0 +1,24 @@
+export const userInit = [
+    {
+        username: 'Tester',
+        email: 'test@gmail.com',
+        password: '12345678'
+    },
+    {
+        username: 'prueba',
+        email: 'prueba@gmail.com',
+        password: '12345678'
+    },
+    {
+        username: 'prueba1',
+        email: 'prueba1@gmail.com',
+        password: '12345678'
+    }
+]
+
+export const login = {
+    email: ' test@gmail.com',
+    password: '12345678'
+}
+
+export const TOKEN = '123456789'
