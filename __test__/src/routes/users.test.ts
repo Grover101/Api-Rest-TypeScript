f2168377072ad7118a4245d17d39d43a808c8431
Grover101 <limachigrover09@gmail.com>
2023-03-05 12:42:05 -0400

    add detaills endpoint test and usersInit in users.test

--- __test__/src/routes/users.test.ts
+++ __test__/src/routes/users.test.ts
@@ -2,24 +2,45 @@ import request from 'supertest'
 
 import app from '@app'
 import { connect, close } from '@config/mongo'
+import { TOKEN } from '../helper/auth'
+import { ResponseMessage, usersInit } from '../helper'
 
 const api = request(app)
 
 beforeAll(async () => {
     jest.setTimeout(60000)
     await connect()
+    await usersInit()
 })
 
 afterAll(async () => {
     await close()
 })
 
+// ! Todo con token
+// [] GET /users/all
 describe('GET /users', () => {
-    test('Retornar todos los users', async () => {
+    test('Peticion de User sin Token', async () => {
         const response = await api
             .get('/api/v1/users')
             .expect('Content-Type', /application\/json/)
+            .expect(403)
+        const { message }: ResponseMessage = await response.body
+        expect('Token is required').toEqual(message)
+    })
+
+    test('Retornar todos los users', async () => {
+        const response = await api
+            .get('/api/v1/users')
+            .auth(TOKEN, { type: 'bearer' })
+            .expect('Content-Type', /application\/json/)
             .expect(200)
+
         expect(Array.isArray(response.body)).toBe(true)
     })
 })
+
+// [ ] GET /users/:id
+// [ ] DELETE /users/:id
+// [ ] PUT /users/profile
+// [ ] POST /users/uploadprofilephoto
