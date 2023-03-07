3550aa8fc3e09b10d558504fd724c7685dfa7879
Grover101 <limachigrover09@gmail.com>
2023-03-06 21:26:31 -0400

    add token in petition

--- __test__/src/routes/tweets.test.ts
+++ __test__/src/routes/tweets.test.ts
@@ -4,14 +4,21 @@ import app from '@app'
 import { connect, close } from '@config/mongo'
 import TweetModel from '@models/tweet'
 import { getAllMessageFromTweets, tweetInit } from '../helper/tweet'
+import { TOKEN } from '../helper/auth'
+import AuthModel, { Auth } from '@models/auth'
+import { authUser } from '../helper'
 
 const api = request(app)
+let token: Auth | null
 
 beforeAll(async () => {
     jest.setTimeout(60000)
     await connect()
     await TweetModel.deleteMany({})
     for (const tweet of tweetInit) await TweetModel.create(tweet)
+    await authUser(api)
+    await AuthModel.deleteMany({})
+    token = await TOKEN()
 })
 
 afterAll(async () => {
@@ -24,6 +31,7 @@ describe('GET all /tweets', () => {
     test('Retornar todos los tweets', async () => {
         const response = await api
             .get('/api/v1/tweets')
+            .auth(token?.token || '', { type: 'bearer' })
             .expect('Content-Type', /application\/json/)
             .expect(200)
         expect(response.body).toHaveLength(tweetInit.length)
--- __test__/src/routes/users.test.ts
+++ __test__/src/routes/users.test.ts
@@ -2,15 +2,21 @@ import request from 'supertest'
 
 import app from '@app'
 import { connect, close } from '@config/mongo'
-import { TOKEN } from '../helper/auth'
-import { ResponseMessage, usersInit } from '../helper'
+import { TOKEN, userInit } from '../helper/auth'
+import { ResponseMessage, authUser, usersInit } from '../helper'
+import AuthModel, { Auth } from '@models/auth'
+import { User } from '@models/user'
 
 const api = request(app)
+let token: Auth | null
 
 beforeAll(async () => {
     jest.setTimeout(60000)
     await connect()
     await usersInit()
+    await authUser(api)
+    await AuthModel.deleteMany({})
+    token = await TOKEN()
 })
 
 afterAll(async () => {
@@ -18,8 +24,8 @@ afterAll(async () => {
 })
 
 // ! Todo con token
-// [] GET /users/all
 describe('GET /users', () => {
+    // [] GET /users/all sin token
     test('Peticion de User sin Token', async () => {
         const response = await api
             .get('/api/v1/users')
@@ -29,14 +35,25 @@ describe('GET /users', () => {
         expect('Token is required').toEqual(message)
     })
 
+    // [] GET /users/all con token
     test('Retornar todos los users', async () => {
         const response = await api
             .get('/api/v1/users')
-            .auth(TOKEN, { type: 'bearer' })
+            .auth(token?.token || '', { type: 'bearer' })
             .expect('Content-Type', /application\/json/)
             .expect(200)
-
         expect(Array.isArray(response.body)).toBe(true)
+    })
+
+    // [] GET /users/all incluede username
+    test('El usuario con username "Tester" se encuentra', async () => {
+        const response = await api
+            .get('/api/v1/users')
+            .auth(token?.token || '', { type: 'bearer' })
+            .expect('Content-Type', /application\/json/)
+            .expect(200)
+        const usernames = response.body.map((user: User) => user.username)
+        expect(usernames).toContain(userInit[0].username)
     })
 })
 
