e7d1b4e548a19861fd951d546295c02aaa20635a
Grover101 <limachigrover09@gmail.com>
2023-03-08 16:19:53 -0400

    add test end point user/:id

--- __test__/src/routes/users.test.ts
+++ __test__/src/routes/users.test.ts
@@ -13,7 +13,7 @@ let token: Auth | null
 beforeAll(async () => {
     jest.setTimeout(60000)
     await connect()
-    await usersInit()
+    // await usersInit()
     await authUser(api)
     await AuthModel.deleteMany({})
     token = await TOKEN()
@@ -57,7 +57,48 @@ describe('GET /users', () => {
     })
 })
 
-// [ ] GET /users/:id
+describe('GET /users/:id', () => {
+    // [] GET /users/:id sin token
+    test('Peticion de User sin Token', async () => {
+        const responseUsers = await api
+            .get('/api/v1/users/')
+            .auth(token?.token || '', { type: 'bearer' })
+        const users: User = await responseUsers.body[0]
+
+        const response = await api
+            .get(`/api/v1/users/${users._id}`)
+            .expect('Content-Type', /application\/json/)
+            .expect(403)
+        const { message }: ResponseMessage = await response.body
+        expect('Token is required').toEqual(message)
+    })
+
+    // [ ] GET /users/:id con token
+    test('Retornar todos los users', async () => {
+        const responseUsers = await api
+            .get('/api/v1/users/')
+            .auth(token?.token || '', { type: 'bearer' })
+        const user: User = await responseUsers.body[0]
+
+        const response = await api
+            .get(`/api/v1/users/${user._id}`)
+            .auth(token?.token || '', { type: 'bearer' })
+            .expect('Content-Type', /application\/json/)
+            .expect(200)
+        expect(response.body.username).toEqual(user.username)
+    })
+
+    // [] GET /users/:id no encontrado
+    test('Usuario no encontrado', async () => {
+        const response = await api
+            .get('/api/v1/users/6408e7c632af07fb2e554d2a')
+            .auth(token?.token || '', { type: 'bearer' })
+            .expect('Content-Type', /application\/json/)
+            .expect(404)
+        expect(response.body.message).toBe('Not Found User')
+    })
+})
+
 // [ ] DELETE /users/:id
 // [ ] PUT /users/profile
 // [ ] POST /users/uploadprofilephoto
