23e67edd942692b0de29e6e468320099dffafcf2
Grover101 <limachigrover09@gmail.com>
2023-04-15 18:19:03 -0400

    add method static find by credentials user

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,9 @@
-import { DocumentType, getModelForClass, prop } from '@typegoose/typegoose'
+import {
+    DocumentType,
+    ReturnModelType,
+    getModelForClass,
+    prop
+} from '@typegoose/typegoose'
 import { TimeStamps } from '@typegoose/typegoose/lib/defaultClasses'
 import bcrypt from 'bcryptjs'
 import { Types } from 'mongoose'
@@ -34,6 +39,18 @@ export class User extends TimeStamps {
 
     public async encryptPassword(this: DocumentType<User>, password: string) {
         this.password = await bcrypt.hash(password, 10)
+    }
+
+    public static async findByCredentials(
+        this: ReturnModelType<typeof User>,
+        email: string,
+        password: string
+    ) {
+        const user = await this.findOne({ email }).exec()
+        if (!user) return null
+        const isPasswordMatch = await bcrypt.compare(password, user.password)
+        if (!isPasswordMatch) return null
+        return user
     }
 }
 
