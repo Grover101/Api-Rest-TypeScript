fb955c93c20f2f2bab333b1115f4a3d945170104
Grover101 <limachigrover09@gmail.com>
2023-05-15 11:48:59 -0400

    add interface type in request

--- src/interfaces/request.ts
+++ src/interfaces/request.ts
@@ -1,6 +1,5 @@
 import { Request } from 'express'
-import { JwtPayload } from 'jsonwebtoken'
 
 export interface RequestExt extends Request {
-    user?: string | JwtPayload
+    user?: string | null
 }
