4f0a72d2a1ec3ec6c08ec02ab6fd85319e339600
Grover101 <limachigrover09@gmail.com>
2023-05-18 11:10:01 -0400

    change middleware validateToken for validateTokenPassUserId

--- src/routes/tweets.ts
+++ src/routes/tweets.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express'
 import tweetController from '@controllers/tweet.controller'
-import { validateToken } from '@middlewares/auth'
+import { validateToken, validateTokenPassUserId } from '@middlewares/auth'
 
 const router = Router()
 
 router.get('/', validateToken, tweetController.index)
 router.get('/:id', validateToken, tweetController.show)
-router.post('/', validateToken, tweetController.create)
-router.post('/like/:id', validateToken, tweetController.createLike)
+router.post('/', validateTokenPassUserId, tweetController.create)
+router.post('/like/:id', validateTokenPassUserId, tweetController.createLike)
 router.put('/:id', validateToken, tweetController.update)
 router.delete('/:id', validateToken, tweetController.delete)
 
